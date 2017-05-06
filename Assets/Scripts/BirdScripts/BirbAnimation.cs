f4454b3b1e2bfd4e72f9f0ba7c8edd683d063aa4
Chyura <mxlxili@gmail.com>
2017-05-06 19:20:25 -0400

    box anim

--- Assets/Scripts/BirdScripts/BirbAnimation.cs
+++ Assets/Scripts/BirdScripts/BirbAnimation.cs
@@ -13,14 +13,20 @@ public class BirbAnimation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.GetKeyDown(KeyCode.F)) {
-			animator.SetInteger("States", 1);
+		if (Input.GetKeyDown (KeyCode.F)) {
+			animator.SetInteger ("States", 1);
 		}
 		if (Input.GetKeyDown (KeyCode.P)) {
 			animator.SetInteger ("States", 2);
 		}
 		if (Input.GetKeyDown (KeyCode.H)) {
 			animator.SetInteger ("States", 0);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			animator.SetBool ("Mirror",false );
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			animator.SetBool ("Mirror", true);
 		}
 		
 	}
