4874409312f17380e9d45cd258f0c7c42accd3f1
Chyura <mxlxili@gmail.com>
2017-05-05 22:32:56 -0400

    added folders

--- Assets/Scripts/PersistentScript.cs
+++ Assets/Scripts/PersistentScript.cs
@@ -5,11 +5,6 @@ public class PersistentScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		DontDestroyOnLoad (transform.gameObject);
 	}
 }
