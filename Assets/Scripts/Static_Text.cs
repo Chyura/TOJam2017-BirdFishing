14dbbf705ed1c1ed9ca7a25341cab2fb48cddf64
Chyura <mxlxili@gmail.com>
2017-05-07 12:31:43 -0400

    changes

--- Assets/Scripts/Static_Text.cs
+++ Assets/Scripts/Static_Text.cs
@@ -2,9 +2,9 @@
 
 public static class Static_Text {
 
-	public static int[] birdnames = ["Pink", "Blue", "Robin", "Chicken", "Owl", "Penguin", "Pig", "Canary", "Goat"]; 
-	public static int[] birdcaught = [0, 0, 0, 0, 0, 0, 0, 0, 0]; 
-	public string[] flavortext = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	public static int[] birdnames = {"Pink", "Blue", "Robin", "Chicken", "Owl", "Penguin", "Pig", "Canary", "Goat"}; 
+	public static int[] birdcaught = {0, 0, 0, 0, 0, 0, 0, 0, 0}; 
+	public string[] flavortext = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 	// Use this for initialization
 	void Start () {
 		
