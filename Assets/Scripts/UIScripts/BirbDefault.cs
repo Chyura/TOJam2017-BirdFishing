4ed8411b57b4f5498445b75e17f3c6f7a08558c2
Chyura <mxlxili@gmail.com>
2017-05-07 16:41:48 -0400

    beginning of collection

---
+++ Assets/Scripts/UIScripts/BirbDefault.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirbDefault : MonoBehaviour {
+	[SerializeField] RuntimeAnimatorController mysteryBirb;
+	[SerializeField] RuntimeAnimatorController actualBirb;
+	private Animator animator;
+	public int birbNum;
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+		animator.runtimeAnimatorController = mysteryBirb as RuntimeAnimatorController;
+		animator.SetInteger ("States", 3);
+	}
+
+	void changeBirb() {
+		animator.runtimeAnimatorController = actualBirb as RuntimeAnimatorController;
+		animator.SetInteger ("States", 3);
+	}
+		
+
+}
