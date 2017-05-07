49197b8ca9a51feacf3db8fe63dcb12060c250e8
lilyxy <lilyw357@gmail.com>
2017-05-07 12:39:57 -0400

    Hi

--- Assets/Scripts/Static_Text.cs
+++ Assets/Scripts/Static_Text.cs
@@ -4,7 +4,7 @@ public class Static_Text {
 
 	public  static string[] birdnames =new string[]{"Pink", "Blue", "Robin", "Chicken", "Owl", "Penguin", "Pig", "Canary", "Goat"};
 	public  static int[] birdcaught = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0}; 
-	public  static string[] flavortext = new string[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	public  static string[] flavortext = new string[] {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 	void Start () {
 		
